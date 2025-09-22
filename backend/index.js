9fa99f56393fb95d33bc634334320a26fa788211
dahamniYoussouf <gy_dahamni@esi.dz>
2025-09-22 07:04:32 -0400

    feat: setup supabase + postgis

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+const restaurant = require('./src/routes/restaurant')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/restaurant',restaurant)
+
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log('App listen on Port'+ port)
+})
